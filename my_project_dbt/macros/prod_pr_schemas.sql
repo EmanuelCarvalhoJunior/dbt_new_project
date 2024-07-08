ca8859eb63c5c5356647853efa125920beeadd18
Emanuel Junior <emanuel.c.junior@hotmail.com>
2024-07-08 14:13:04 -0300

    Melhoria no macro dbt

--- my_project_dbt/macros/prod_pr_schemas.sql
+++ my_project_dbt/macros/prod_pr_schemas.sql
@@ -2,28 +2,32 @@
 
     {% do log('Number PR: ' ~  PR_number  ,True) %}
 
-    {% set number_stg = PR_number|string %}
-    {% set command_list = [] %}
-    {% set pr_cleanup_query %}
-        SHOW SCHEMAS IN DATABASE DBT_DB;
-    {% endset %}
+    {% if PR_number%}
+        {% set command_list = [] %}
+        {% set pr_cleanup_query %}
+            SHOW SCHEMAS IN DATABASE DBT_DB;
+        {% endset %}
 
-    {% set result = run_query(pr_cleanup_query).columns[1].values() %}
-    {% if result %}
-        {% for item in result %} 
-            {{ command_list.append(item) }}
-        {% endfor %}
-    {% endif %}
+        {% set result = run_query(pr_cleanup_query).columns[1].values() %}
+        {% if result %}
+            {% for item in result %} 
+                {{ command_list.append(item) }}
+            {% endfor %}
+        {% endif %}
 
-    {% if command_list %}
-        {% for item in command_list %}
-            {% if number_stg in item %}
-                {% do log('drop schema if exists ' ~ item ~ ' cascade',True ) %}
-                {% do run_query('drop schema if exists ' ~ item ~ ' cascade') %}
-            {% endif %} 
-        {% endfor %}
+        {% if command_list %}
+            {% for item in command_list %}
+                {% if PR_number in item %}
+                    {% do log('drop schema if exists ' ~ item ~ ' cascade',True ) %}
+                    {% do run_query('drop schema if exists ' ~ item ~ ' cascade') %}
+                {% endif %} 
+            {% endfor %}
+        {% else %}
+            {% do log('No schemas to drop',info=True) %}
+        {% endif %}
+
     {% else %}
-        {% do log('No schemas to drop',info=True) %}
+        {% do log('PR Number not found',True) %}
     {% endif %}
 
 {%- endmacro -%}
